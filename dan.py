408e89d998f62a4c5c647b28fd599f7905125a98
daniilios <127761434+daniilios@users.noreply.github.com>
2023-04-26 15:08:05 +0300

    Update dan.py

--- dan.py
+++ dan.py
@@ -1,3 +1,234 @@
-import subprocess
+import json
+import urllib.request
+import webbrowser
+import os
 
-subprocess.call(['mkdir', '/mnt/c/main'])
+
+R = '\033[91m'
+Y = '\033[93m'
+G = '\033[92m'
+CY = '\033[96m'
+W = '\033[97m'
+path = os.path.isfile('/data/data/com.termux/files/usr/bin/bash')
+
+
+
+
+
+
+
+
+def vic():
+    print( Y + '''
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░██████░░███░░░░███░░████████░░░░
+░░░██░░░██░████░░████░░░░░██░░░░░░░
+░░░██░░░██░██░████░██░░░░░██░░░░░░░
+░░░██████░░██░░██░░██░░░░░██░░░░░░░
+░░░██░░░░░░██░░░░░░██░░░░░██░░░░░░░
+░░░██░░░░░░██░░░░░░██░░░░░██░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+''' + R )
+
+vic()
+
+
+def menu():
+    print("""
+[1] информация по ip
+[2] генерация ссылки iploger(узнать ip через переход по ссылке)
+    
+    """)
+menu()
+
+def main2():
+    print('''
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░███░░░█████░░░█████░░███░░░░░██░░░░░
+░░░░░░█░░░█░██░░░██░██░░░██░████░░░░██░░░░░
+░░░░░░█░░░░░█░░░░░█░█░░░░░█░██░██░░░██░░░░░
+░░░░░░░███░░█░░░░░█░█░░░░░█░██░░██░░██░░░░░
+░░░░░░░░░░█░█░░░░░█░█░░░░░█░██░░░██░██░░░░░
+░░░░░░█░░░█░██░░░██░██░░░██░██░░░░████░░░░░
+░░░░░░░███░░░█████░░░█████░░██░░░░░███░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+[1] выйти
+
+''')
+
+
+def menu3():
+    ch2 = int(input("выберете катерегорию: "))
+    if ch2 == 1:
+        vic()
+        menu()
+        menu2()
+def menu2():
+
+    ch = int(input("выберете катерегорию: "))
+
+    if ch == 1:
+        main1()
+    elif ch == 2:
+        main2()
+
+
+
+
+
+
+
+
+
+
+
+def main1():
+    import json
+    import urllib.request
+    import webbrowser
+    import os
+    try:
+        R = '\033[91m'
+        Y = '\033[93m'
+        G = '\033[92m'
+        CY = '\033[96m'
+        W = '\033[97m'
+        path = os.path.isfile('/data/data/com.termux/files/usr/bin/bash')
+
+        def start():
+            os.system('clear')
+            print(CY + """
+     _ ______ _       _                 
+    | (_____ (_)     | |                
+    | |_____) )  ____| |  _ _____  ____ 
+    | |  ____/ |/ ___) |_/ ) ___ |/ ___)
+    | | |    | ( (___|  _ (| ____| |    
+    |_|_|    |_|\____)_| \_)_____)_|   """ + Y + """v1.1""" + G + """
+
+
+         Simple IP Address locator
+
+    """ + R + """>>""" + Y + """----""" + CY + """ Developers - Danilians """ + Y + """----""" + R + """<<""")
+
+        def m3():
+            try:
+                print(R + """\n
+    #""" + Y + """ выберете раздел""" + G + """ >>""" + Y + """
+
+    1)""" + G + """информация об вашем IP""" + Y + """
+    2)""" + G + """узнать информацию о чужом IP""" + Y + """
+    3)""" + G + """ вернутся в меню
+    """)
+                ch = int(input(CY + "Ващ выбор: " + W))
+                if ch == 1:
+                    main2()
+                    m3()
+                elif ch == 2:
+                    main()
+                    m3()
+                elif ch == 3:
+                    print("Выход................")
+                    vic()
+                    menu()
+                    menu2()
+
+                else:
+                    print(R + "\nНеизвестная команда\n")
+                    m3()
+            except ValueError:
+                print(R + "\nНеизвестная команда поробуйде еще раз \n")
+                m3()
+
+        def finder(u):
+            try:
+                try:
+                    response = urllib.request.urlopen(u)
+                    data = json.load(response)
+
+                    print(R + "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    print(Y + '\n>>>' + CY + ' детали IP\n ')
+                    print(G + "1) IP адрес : " + Y, data['query'], '\n')
+                    print(G + "2) Провайдер : " + Y, data['org'], '\n')
+                    print(G + "3) Город : " + Y, data['city'], '\n')
+                    print(G + "4) Регион : " + Y, data['regionName'], '\n')
+                    print(G + "5) Страна : " + Y, data['country'], '\n')
+                    print(G + "6) Локация\n")
+                    print(G + "\tДолгота : " + Y, data['lat'], '\n')
+                    print(G + "\tШирина : " + Y, data['lon'], '\n')
+                    l = 'https://www.google.com/maps/place/' + str(data['lat']) + '+' + str(data['lon'])
+                    print(R + "\n#" + Y + "ссылка на Google : " + CY, l)
+                    path = os.path.isfile('/data/data/com.termux/files/usr/bin/bash')
+                    if path:
+                        link = 'am start -a android.intent.action.VIEW -d ' + str(l)
+                        pr = input(R + "\n>>" + Y + " Открыть ссылку в вашем браузере?" + G + " (y|n): " + W)
+                        if pr == "y":
+                            lnk = str(link) + " > /dev/null"
+                            os.system(str(lnk))
+                            start()
+                            m3()
+                        elif pr == "n":
+                            print("\nCheck another IP or exit using Ctrl + C\n\n")
+                            start()
+                            m3()
+                        else:
+                            print("\nInvalid choice! Please try again\n")
+                            m3()
+                    else:
+                        pr = input(R + "\n>>" + Y + " Открыть ссылку в вашем браузере?" + G + " (y|n): " + W)
+                        if pr == "y":
+                            webbrowser.open(l, new=0)
+                            start()
+                            m3()
+                        elif pr == "n":
+                            print(R + "\n++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                            print(Y + "\nCheck another IP or exit using " + R + "Ctrl + C\n\n")
+                            start()
+                            m3()
+                        else:
+                            print(R + "\nInvalid choice! Please try again\n")
+                            m3()
+                    return
+                except KeyError:
+                    print(R + "\nError! Неверный IP/вебсайт Адрес!\n" + W)
+                    m3()
+            except urllib.error.URLError:
+                print(R + "\nError!" + Y + " Пожалуйста проверте свое интернет сединение!\n" + W)
+                exit()
+
+        def main():
+            u = input(G + "\n>>> " + Y + "Введите IP Адрес/вебсайт Адрес:" + W + " ")
+            if u == "":
+                print(R + "\nEnter valid IP Address/website address!")
+                main()
+            else:
+                url = 'http://ip-api.com/json/' + u
+                finder(url)
+
+        def main2():
+            url = 'http://ip-api.com/json/'
+            finder(url)
+
+        start()
+        m3()
+    except KeyboardInterrupt:
+        print(Y + "\Хорошего дня :)" + W)
+
+menu2()
+
+def main2():
+    print('''
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░░░░░░░███░░░█████░░░█████░░███░░░░░██░░░░░
+░░░░░░█░░░█░██░░░██░██░░░██░████░░░░██░░░░░
+░░░░░░█░░░░░█░░░░░█░█░░░░░█░██░██░░░██░░░░░
+░░░░░░░███░░█░░░░░█░█░░░░░█░██░░██░░██░░░░░
+░░░░░░░░░░█░█░░░░░█░█░░░░░█░██░░░██░██░░░░░
+░░░░░░█░░░█░██░░░██░██░░░██░██░░░░████░░░░░
+░░░░░░░███░░░█████░░░█████░░██░░░░░███░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+''')
+
+
+
+menu3()
